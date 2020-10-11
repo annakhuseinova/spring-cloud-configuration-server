7389947e0e8ab0cfd5438a125e1c1423068e7d9f
annakhuseinova <annakhuseinova@yandex.ru>
2020-10-11 21:48:15 +0300

    Initialized spring cloud config repo

---
+++ src/main/java/com/annakhuseinova/springcloudconfigurationserver/SpringCloudConfigurationServerApplication.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springcloudconfigurationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigurationServerApplication.class, args);
+	}
+
+}
